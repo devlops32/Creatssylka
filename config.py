67d42b9eb4e6dec5775d5c04719e00f908092ca2
devlopss <jensbens67@gmail.com>
2026-08-23 11:01:47 +0300

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = "8877267071:AAGsoJ5j4mw09NBHdlBfl1om0aqqRolANBM"
-WEB_HOST = "0.0.0.0"
-WEB_PORT = 8080
-BASE_URL = "http://ВАШ_IP:8080"  # Для локального теста localhost:8080
-PHOTO_FOLDER = "photos"
-LINK_LIFETIME = 300  # 5 минут
