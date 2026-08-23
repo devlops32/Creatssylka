7b8737e5f5c8912e0228b70040fadf62959a2b83
devlopss <jensbens67@gmail.com>
2026-08-23 11:02:19 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "8877267071:AAGsoJ5j4mw09NBHdlBfl1om0aqqRolANBM"
+WEB_HOST = "0.0.0.0"
+WEB_PORT = 8080
+BASE_URL = "http://127.0.0.1:8080"  # Для локального теста localhost:8080
+PHOTO_FOLDER = "photos"
+LINK_LIFETIME = 300  # 5 минут
---
+++ web_server.py
@@ -0,0 +1,48 @@
+from aiohttp import web
+import os
+import asyncio
+from datetime import datetime, timedelta
+from config import WEB_HOST, WEB_PORT, PHOTO_FOLDER, LINK_LIFETIME  # <-- ЭТО БЫЛО ПРОПУЩЕНО
+
+links = {}  # {link_id: (filepath, expire_time)}
+
+async def handle_photo(request):
+    link_id = request.match_info.get('id')
+    if link_id not in links:
+        return web.Response(text="Ссылка истекла или не существует", status=404)
+    
+    filepath, expire = links[link_id]
+    if datetime.now() > expire:
+        del links[link_id]
+        try:
+            os.remove(filepath)
+        except:
+            pass
+        return web.Response(text="Ссылка истекла", status=404)
+    
+    return web.FileResponse(filepath)
+
+async def cleanup_loop():
+    while True:
+        await asyncio.sleep(60)
+        now = datetime.now()
+        to_delete = [k for k, (fp, exp) in links.items() if now > exp]
+        for k in to_delete:
+            try:
+                os.remove(links[k][0])
+            except:
+                pass
+            del links[k]
+
+def start_web():
+    os.makedirs(PHOTO_FOLDER, exist_ok=True)
+    app = web.Application()
+    app.router.add_get('/photo/{id}', handle_photo)
+    runner = web.AppRunner(app)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(runner.setup())
+    site = web.TCPSite(runner, WEB_HOST, WEB_PORT)
+    loop.run_until_complete(site.start())
+    loop.create_task(cleanup_loop())
+    loop.run_forever()
